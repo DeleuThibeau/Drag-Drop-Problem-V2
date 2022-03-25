fc3f66074be63cdae3b99533d67d8a5c1681d9d5
Thibeau <thibeau.deleu@student.howest.be>
2022-03-25 15:38:03 +0100

    Setup problem complete

--- Drag-Drop-Problem-V2/src/app/app.component.ts
+++ Drag-Drop-Problem-V2/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'Drag-Drop-Problem-V2';
+  public items: string[] = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+  ];
+
+  drop(e: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, e.previousIndex, e.currentIndex);
+  }
 }
