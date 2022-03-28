4b286ae1daafd5af4172f34ab0743950c055b6c1
Thibeau <thibeau.deleu@student.howest.be>
2022-03-28 16:19:34 +0200

    Cleanup code

--- Drag-Drop-Problem-V2/src/app/app.component.ts
+++ Drag-Drop-Problem-V2/src/app/app.component.ts
@@ -14,7 +14,7 @@ import {
 export class AppComponent {
   @ViewChild('dropListContainer') dropListContainer?: ElementRef;
 
-  public items: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  public items: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   dropListReceiverElement?: HTMLElement;
   dragDropInfo?: {
